f54ee36b2ae80d8b7db165d27a69ca76bc420c78
Monika Wieczorek <monikawieczoreq@gmail.com>
2020-03-31 17:01:21 +0200

    Add rounding numbers

--- js/script.js
+++ js/script.js
@@ -2,28 +2,33 @@ const celsiusInput = document.getElementById('celsius_inp');
 const fahrenheitInput = document.getElementById('fahrenheit_inp');
 const kelvinInput = document.getElementById('kelvin_inp');
 
+function roundNum(num){
+  return Math.round(num*100)/100;
+  //return Math.round((num*100)/100).toFixed(2);
+}
+
 function celsiusToFahrenheitAndKelvin(){
   const cTemp = parseFloat(celsiusInput.value);
   const fTemp = (cTemp * (9/5)) + 32;
   const kTemp = cTemp + 273.15;
-  fahrenheitInput.value = fTemp;
-  kelvinInput.value = kTemp;
+  fahrenheitInput.value = roundNum(fTemp);
+  kelvinInput.value = roundNum(kTemp);
 }
 
 function fahrenheitToCelsiusAndKelvin(){
   const fTemp = parseFloat(fahrenheitInput.value);
   const cTemp = (fTemp - 32) * (5/9);
   const kTemp = (fTemp + 459.67) * 5/9;
-  celsiusInput.value = cTemp;
-  kelvinInput.value = kTemp;
+  celsiusInput.value = roundNum(cTemp);
+  kelvinInput.value = roundNum(kTemp);
 }
 
 function kelvinToCelsiusAndFahrenheit(){
   const kTemp = parseFloat(kelvinInput.value);
   const cTemp = kTemp - 273.15;
   const fTemp = 9/5 * (kTemp - 273) + 32;
-  celsiusInput.value = cTemp;
-  fahrenheitInput.value = fTemp;
+  celsiusInput.value = roundNum(cTemp);
+  fahrenheitInput.value = roundNum(fTemp);
 }
 
 celsiusInput.addEventListener('input', celsiusToFahrenheitAndKelvin);
